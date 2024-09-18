4f3e418657bdce4b0b829ef6eaf2b75d00a59d11
Juan Silva <juanse909@hotmail.com>
2024-09-18 08:09:57 -0500

    data

---
+++ src/data/dataUsers.ts
@@ -0,0 +1,9 @@
+export const dataUsers = [
+    {
+        id: 0,
+        background: '../src/assets/user/background/mauricio-santos-N1gFsYf9AI0-unsplash.jpg',
+        userpic: '../src/assets/user/pics/dwayne-joe-LTY3S3mFHGY-unsplash.jpg',
+        name: 'Mara Volkov',
+        at: '@MaraVolkov',
+    }
+]
