906bc5a87fefdd877a7ab3eaad7253c9cf1313b3
Juan Silva <juanse909@hotmail.com>
2024-09-21 19:32:59 -0500

    update

--- src/components/userInfo/userInfo.ts
+++ src/components/userInfo/userInfo.ts
@@ -36,15 +36,17 @@ class UserInfo extends HTMLElement {
             <link rel="stylesheet" href="../src/components/userInfo/userInfo.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
             <section class='container'>
-                <div class='container-background'>
-                    <img src="${this.background || 'No Image'}" alt="Background">
-                        <div class='container-userpic'>
-                            <img src="${this.userpic || 'No Image'}" alt="User Picture">
-                        </div>
-                        <div class='container-text'>
-                            <h2>${this.name || 'No Name'}</h2>
-                            <p>${this.at || 'No At'}</p>
-                        </div>
+            <img src="${this.background || 'No Image'}" alt="Background">
+            <div class='container-background'>
+            <div class='container-userpic'>
+                <a href="#">
+                    <img src="${this.userpic || 'No Image'}" alt="User Picture">
+                </a>
+                    </div>
+                    <div class='container-text'>
+                        <h2>${this.name || 'No Name'}</h2>
+                        <p>${this.at || 'No At'}</p>
+                    </div>
                 </div>
             </section>
             `;
