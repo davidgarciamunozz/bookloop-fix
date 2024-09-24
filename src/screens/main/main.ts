2c69ba5f5e73d60b21713c8d246c021ea7ed2919
Juan Silva <juanse909@hotmail.com>
2024-09-24 00:16:11 -0500

    update

--- src/screens/main/main.ts
+++ src/screens/main/main.ts
@@ -2,15 +2,20 @@ import * as components from '../../components/index';
 import '../../components/userInfo/userInfo';
 import '../../components/navBar/navBar';
 import '../../components/userMenu/userMenu';
+import '../../components/newPost/newPost';
 import UserInfo, { Attribute } from '../../components/userInfo/userInfo';
 import { dataUsers } from '../../data/dataUsers';
 
 class Main extends HTMLElement {
     user: UserInfo[] = [];
+    currentUserPic: string = ''; // TEMPORARY variable to store the current user's image
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+
+        // We assume that the first user in dataUsers is the current user
+        this.currentUserPic = dataUsers[0].userpic;
 
         dataUsers.forEach(dataUser => {
             const userCard = this.ownerDocument.createElement('user-info') as UserInfo;
@@ -28,10 +33,14 @@ class Main extends HTMLElement {
 
     render() {
         if (this.shadowRoot) {
+            
             const navBar = this.ownerDocument.createElement('nav-bar');
             navBar.setAttribute('icon', "../src/assets/logos/big_logo.png");
             navBar.setAttribute('img', "../src/assets/logos/medium_logo.png");
             navBar.setAttribute('input', "Search");
+            
+            const container = this.ownerDocument.createElement('section');
+            container.className = 'container';
 
             const userContainer = this.ownerDocument.createElement('section');
             userContainer.className = 'user-container';
@@ -48,12 +57,29 @@ class Main extends HTMLElement {
             userMenu.setAttribute('setting', '#');
 
             userContainer.appendChild(userMenu);
+            container.appendChild(userContainer);
+
+            const postContainer = this.ownerDocument.createElement('section');
+            postContainer.className = 'post-container';
+
+            const newPost = this.ownerDocument.createElement('new-post');
+            newPost.setAttribute('userpic', this.currentUserPic);
+            newPost.setAttribute('text', 'Create new post');
+            newPost.setAttribute('buttontext', 'Text');
+            newPost.setAttribute('buttonimages', 'Images');
+            newPost.setAttribute('inputtext', 'Share something...');
+            newPost.setAttribute('inputimage', 'Drag and drop or upload media');
+            newPost.setAttribute('club', 'Select a Club');
+            newPost.setAttribute('post', 'Post');
+
+            postContainer.appendChild(newPost);
+            container.appendChild(postContainer);
 
             this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="../src/screens/main/main.css">
             `;
             this.shadowRoot.appendChild(navBar);
-            this.shadowRoot.appendChild(userContainer);
+            this.shadowRoot.appendChild(container);
         }
     }    
 }
