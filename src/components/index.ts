a480503c4759f0782ee9c1b17ee05c9a6e4baf3e
Juan Silva <juanse909@hotmail.com>
2024-09-24 00:15:21 -0500

    father

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from './navBar/navBar';
 export { default as UserInfo } from './userInfo/userInfo';
 export { default as UserMenu } from './userMenu/userMenu';
+export { default as NewPost } from './newPost/newPost';
