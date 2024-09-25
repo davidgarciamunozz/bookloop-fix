56408ebf4b72742c75102efd97ceecce5a2298d0
Majo Rodríguez <mariajorm2004@gmail.com>
2024-09-24 22:50:57 -0500

    feat: data

---
+++ src/data/dataClubs.ts
@@ -0,0 +1,24 @@
+export const dataClubs = [
+    {
+        uid: 0,
+        name: 'Harry Potter',
+        bannerpic: '../src\assets\clubs\potterbanner.webp',
+        clubpic: '../src\assets\clubs\harry-potterclubpic.webp',
+        members: 2938293
+    }, 
+    {
+        uid: 1,
+        name: 'The Great Gatsby',
+        bannerpic: '../src\assets\clubs\greatgatsbybanner.webp',
+        clubpic: '../src\assets\clubs\greatgatsbyclubpic.jpg',
+        members: 353223
+    },
+    {
+        uid: 2,
+        name: 'Harry Potter',
+        bannerpic: '../src\assets\clubs\prideandprejudice-banner.webp',
+        clubpic: '../src\assets\clubs\prideandprejudice-clubpic.webp',
+        members: 2938293
+    },
+
+]
