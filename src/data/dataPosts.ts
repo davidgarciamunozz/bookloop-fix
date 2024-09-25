7441e35db99a2d71556a531932eb2d65183f6059
Majo Rodríguez <mariajorm2004@gmail.com>
2024-09-25 00:22:56 -0500

    fix: no longer bugged data

--- src/data/dataPosts.ts
+++ src/data/dataPosts.ts
@@ -1,28 +1,28 @@
 export const dataPosts = [
     {
-        clubPic: "../src\assets\clubs\greatgatsbyclubpic.jpg",
+        clubPic: "../src/assets/clubs/greatgatsbyclubpic.jpg",
         clubName: "The great Gatsby",
-        image: "../src\assets\dataIMG\post1.jpg",
+        image: "../src/assets/dataIMG/post1.jpg",
         likes: 20,
         comments: 7,
         author: "Mara Volkov",
         desc: "Look at my new collection!"
     },
     {
-        clubPic: "../src\assets\clubs\greatgatsbyclubpic.jpg",
+        clubPic:"../src/assets/clubs/greatgatsbyclubpic.jpg",
         clubName: "The great Gatsby",
-        image: "../src\assets\dataIMG\post2.jpg",
-        likes: 20,
-        comments: 7,
+        image: "../src/assets/dataIMG/post2.jpg",
+        likes: 921,
+        comments: 300,
         author: "Luisa Booker",
         desc: "Just keep on mind whatever he wanted to tell us with this quote..."
     },
     {
-        clubPic: "../src\assets\clubs\greatgatsbyclubpic.jpg",
+        clubPic: "../src/assets/clubs/greatgatsbyclubpic.jpg",
         clubName: "The great Gatsby",
-        image: "../src\assets\dataIMG\post3.jpg",
-        likes: 20,
-        comments: 7,
+        image: "../src/assets/dataIMG/post3.jpg",
+        likes: 2983,
+        comments: 892,
         author: "Mark123",
         desc: "Do yall ever wonder what could've happened if they never kissed?"
     },
