e8f5a1228c88092a8c37490d9edfce36d136208b
Majo Rodríguez <mariajorm2004@gmail.com>
2024-09-26 00:41:06 -0500

    refactor: data mispelling

--- src/data/dataClubs.ts
+++ src/data/dataClubs.ts
@@ -2,22 +2,22 @@ export const dataClubs = [
     {
         uid: 0,
         name: 'Harry Potter',
-        bannerpic: '../src\assets\clubs\potterbanner.webp',
-        clubpic: '../src\assets\clubs\harry-potterclubpic.webp',
+        bannerpic: '../src/assets/clubs/potterbanner.webp',
+        clubpic: '../src/assets/clubs/harry-potterclubpic.webp',
         members: 2938293
     }, 
     {
         uid: 1,
         name: 'The Great Gatsby',
-        bannerpic: '../src\assets\clubs\greatgatsbybanner.webp',
-        clubpic: '../src\assets\clubs\greatgatsbyclubpic.jpg',
+        bannerpic: '../src/assets/clubs/greatgatsbybanner.webp',
+        clubpic: '../src/assets/clubs/greatgatsbyclubpic.jpg',
         members: 353223
     },
     {
         uid: 2,
         name: 'Harry Potter',
-        bannerpic: '../src\assets\clubs\prideandprejudice-banner.webp',
-        clubpic: '../src\assets\clubs\prideandprejudice-clubpic.webp',
+        bannerpic: '../src/assets/clubs/prideandprejudice-banner.webp',
+        clubpic: '../src/assets/clubs/prideandprejudice-clubpic.webp',
         members: 2938293
     },
 
