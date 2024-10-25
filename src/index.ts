b6f9d192dcbe5c0071a3fbac497a0f92dfd6dd3c
Juan Silva <juanse909@hotmail.com>
2024-10-25 00:34:05 -0500

    refactor: index working with store

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,43 @@
 import * as components from './components/index';
+import { addObserver, appState  } from './store/index';
+import { Screens } from './types/store';
 import './screens/DASHBOARD/DASHBOARD';
 
 class AppContainer extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		addObserver(this);
+	}
 
-    connectedCallback() {
-        this.render();
-    }
+	connectedCallback() {
+		this.render();
+	}
 
-    render() {
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML = `
-            <dashboard-page></dashboard-page>
-            `;
-        }
-    }
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = '';
+			switch (appState.screen) {
+				case Screens.REGISTER:
+					const register = this.ownerDocument.createElement('app-register');
+					this.shadowRoot.appendChild(register);
+					break;
+
+				case Screens.LOGIN:
+					const login = this.ownerDocument.createElement('app-login');
+					this.shadowRoot.appendChild(login);
+					break;
+
+				case Screens.DASHBOARD:
+					const dashboard = this.ownerDocument.createElement('app-dashboard');
+					this.shadowRoot.appendChild(dashboard);
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
 }
 
 customElements.define('app-container', AppContainer);
