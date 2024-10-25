830770cb15f4f75aa19ce7a023f63773aa69d71b
Juan Silva <juanse909@hotmail.com>
2024-10-25 00:02:08 -0500

    fix: documentation order

--- src/screens/main/main.ts
+++ src/screens/main/main.ts
@@ -9,23 +9,17 @@ import { dataUsers } from '../../data/dataUsers';
 import Post, { Attribute2 } from '../../components/postComponent/post';
 import { dataPosts } from '../../data/dataPosts';
 import '../../components/postPopUp/postPopUp';
-import PostPopUp, { Attribute3} from '../../components/postPopUp/postPopUp';
+import PostPopUp, { Attribute3 } from '../../components/postPopUp/postPopUp';
 import '../../components/elements/clubInfo/clubInfo';
 import '../../components/clubsCard/clubsCard';
 import ClubsCard, { AttributeClubsCard } from '../../components/clubsCard/clubsCard';
 import { dataClubs } from '../../data/dataClubs';
 
-
-
-
-
-
-
 class Main extends HTMLElement {
     user: UserInfo[] = [];
     post: Post[] = [];
-    currentUserPic: string = ''; 
-    isUserContainerVisible: boolean = true; 
+    currentUserPic: string = '';
+    isUserContainerVisible: boolean = true;
 
     constructor() {
         super();
@@ -54,11 +48,11 @@ class Main extends HTMLElement {
         const userContainer = this.shadowRoot?.querySelector('.user-container');
         const postContainer = this.shadowRoot?.querySelector('.post-container');
         const container = this.shadowRoot?.querySelector('.container');
-        
+
         if (userContainer && postContainer && container) {
             // Ocultar/Mostrar el user-container
             userContainer.classList.toggle('hidden', !this.isUserContainerVisible);
-    
+
             // Expandir el post-container y el container principal si user-container está oculto
             if (!this.isUserContainerVisible) {
                 postContainer.classList.add('expanded');
@@ -69,7 +63,7 @@ class Main extends HTMLElement {
             }
         }
     }
-    
+
     render() {
         if (this.shadowRoot) {
             const navBar = this.ownerDocument.createElement('nav-bar');
@@ -110,59 +104,59 @@ class Main extends HTMLElement {
             newPost.setAttribute('inputimage', 'Drag and drop or upload media');
             newPost.setAttribute('club', 'Select a Club');
             newPost.setAttribute('post', 'Post');
-            
+
             postContainer.appendChild(newPost);
             container.appendChild(postContainer);
-        
+
             // Posts & pop up
-        const postDashboard = this.ownerDocument.createElement('section');
-postDashboard.className = 'post-dashboard';
+            const postDashboard = this.ownerDocument.createElement('section');
+            postDashboard.className = 'post-dashboard';
 
-if (dataPosts && Array.isArray(dataPosts)) {
-    dataPosts.forEach(dataPost => {
-        const post = this.ownerDocument.createElement('post-component') as Post;
+            if (dataPosts && Array.isArray(dataPosts)) {
+                dataPosts.forEach(dataPost => {
+                    const post = this.ownerDocument.createElement('post-component') as Post;
 
-        post.setAttribute('clubpic', dataPost.clubpic);
-        post.setAttribute('clubname', dataPost.clubname);
-        post.setAttribute('image', dataPost.image);
-        post.setAttribute('likes', dataPost.likes.toString());
-        post.setAttribute('comments', dataPost.comments.toString());
-        post.setAttribute('author', dataPost.author);
-        post.setAttribute('desc', dataPost.desc);
+                    post.setAttribute('clubpic', dataPost.clubpic);
+                    post.setAttribute('clubname', dataPost.clubname);
+                    post.setAttribute('image', dataPost.image);
+                    post.setAttribute('likes', dataPost.likes.toString());
+                    post.setAttribute('comments', dataPost.comments.toString());
+                    post.setAttribute('author', dataPost.author);
+                    post.setAttribute('desc', dataPost.desc);
 
-        
 
-        postDashboard.appendChild(post);
-        this.post.push(post);
-    });
-} else {
-    console.error('dataPosts is not an array or is undefined');
-}
 
-postContainer.appendChild(postDashboard);
+                    postDashboard.appendChild(post);
+                    this.post.push(post);
+                });
+            } else {
+                console.error('dataPosts is not an array or is undefined');
+            }
 
-// clubs
-const clubsContainer = this.ownerDocument.createElement('section');
-clubsContainer.className = 'clubs-container';
+            postContainer.appendChild(postDashboard);
 
-if (dataClubs && Array.isArray(dataClubs)) {
-    const clubsCard1 = this.ownerDocument.createElement('clubs-card') as ClubsCard;
-    clubsCard1.setAttribute('cardtitle', 'Clubs');
-    clubsCard1.setAttribute('buttontext', 'Your Clubs');
-    clubsCard1.setAttribute('cardcolor', '#6471C7');
-    clubsCard1.setAttribute('buttoncolor', '#6471C7');
-    clubsContainer.appendChild(clubsCard1);
+            // clubs
+            const clubsContainer = this.ownerDocument.createElement('section');
+            clubsContainer.className = 'clubs-container';
 
-    const clubsCard2 = this.ownerDocument.createElement('clubs-card') as ClubsCard;
-    clubsCard2.setAttribute('cardtitle', 'Discover');
-    clubsCard2.setAttribute('buttontext', 'Discover now');
-    clubsCard2.setAttribute('cardcolor', '#C2BE4D');
-    clubsCard2.setAttribute('buttoncolor', '#C2BE4D');
-    clubsContainer.appendChild(clubsCard2);
-}
+            if (dataClubs && Array.isArray(dataClubs)) {
+                const clubsCard1 = this.ownerDocument.createElement('clubs-card') as ClubsCard;
+                clubsCard1.setAttribute('cardtitle', 'Clubs');
+                clubsCard1.setAttribute('buttontext', 'Your Clubs');
+                clubsCard1.setAttribute('cardcolor', '#6471C7');
+                clubsCard1.setAttribute('buttoncolor', '#6471C7');
+                clubsContainer.appendChild(clubsCard1);
 
-console.log(clubsContainer);
-container.appendChild(clubsContainer);
+                const clubsCard2 = this.ownerDocument.createElement('clubs-card') as ClubsCard;
+                clubsCard2.setAttribute('cardtitle', 'Discover');
+                clubsCard2.setAttribute('buttontext', 'Discover now');
+                clubsCard2.setAttribute('cardcolor', '#C2BE4D');
+                clubsCard2.setAttribute('buttoncolor', '#C2BE4D');
+                clubsContainer.appendChild(clubsCard2);
+            }
+
+            console.log(clubsContainer);
+            container.appendChild(clubsContainer);
 
 
             this.shadowRoot.innerHTML = `
