4104ef3bb7cbe04993810586996e1c7c2080b3c5
Juan Silva <juanse909@hotmail.com>
2024-10-30 01:19:40 -0500

    refractor: modification of the appState to work with the authentication process

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,39 @@
 import { reducer } from './reducer';
 import Storage from '../utils/storage';
-import { AppState, Observer } from '../types/store';
+import { AppState, Observer, Screens } from '../types/store';
+import { onAuthStateChanged } from 'firebase/auth';
+import { getFirebaseInstance } from '../utils/firebase';
+import { navigate, setUserCredentials } from './actions';
+
+const onAuth = async () => {
+	const { auth } = await getFirebaseInstance();
+	onAuthStateChanged(auth, (user) => {
+		if (user){
+			user.uid !== null ? dispatch(setUserCredentials(user.uid)) : '';
+			dispatch(navigate(Screens.DASHBOARD));
+		}else{
+			dispatch(navigate(Screens.LOGIN));
+		};
+	});
+};
+
+onAuth();
 
 const initialState: AppState = {
-	screen: 'DISCOVERLANDING',
+	screen: 'LOGIN',
 	publications: [],
+	user: {},
 };
 
-export let appState = Storage.get('STORE', initialState);
+export let appState = initialState;
 
 let observers: Observer[] = [];
-
-const persistStore = (state: any) => {
-	Storage.set('STORE', state);
-};
 
 export const dispatch = (action: any) => {
 	const clone = JSON.parse(JSON.stringify(appState));
 	const newState = reducer(action, clone);
 	appState = newState;
 
-	persistStore(newState);
 	observers.forEach((o: any) => o.render());
 };
 
