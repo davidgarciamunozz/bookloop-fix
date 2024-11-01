be2220090a724ce45ff2a7d176989a2ee5afc765
Juan Silva <juanse909@hotmail.com>
2024-11-01 17:44:22 -0500

    fix: key of object data

--- src/data/dataUsers.ts
+++ src/data/dataUsers.ts
@@ -4,6 +4,6 @@ export const dataUsers = [
         background: '../src/assets/user/background/mauricio-santos-N1gFsYf9AI0-unsplash.jpg',
         userpic: '../src/assets/user/pics/dwayne-joe-LTY3S3mFHGY-unsplash.jpg',
         name: 'Mara Volkov',
-        at: '@MaraVolkov',
+        username: '@MaraVolkov',
     }
 ]
