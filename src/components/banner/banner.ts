2dc2276fbfb72ce2759e75187d71366d0b3652d7
Juan Silva <juanse909@hotmail.com>
2024-11-01 14:49:35 -0500

    feat: indicate the desired color when creating the component

--- src/components/banner/banner.ts
+++ src/components/banner/banner.ts
@@ -36,37 +36,37 @@ class Banner extends HTMLElement {
             link.href = '../src/components/banner/banner.css';
             this.shadowRoot.appendChild(link);
 
-            // Create the main container (section)
             const container = this.ownerDocument.createElement('section');
             container.className = 'container';
+
+            const bgColor = this.getAttribute('bg-color');
+            if (bgColor) {
+                container.style.setProperty('--banner-bg-color', bgColor);
+            }
+
             this.shadowRoot.appendChild(container);
 
-            // Create the container for banner text
             const textContainer = this.ownerDocument.createElement('div');
             textContainer.className = 'action-user-element';
             container.appendChild(textContainer);
 
-            // Create title element
             const title = this.ownerDocument.createElement('h1');
             title.className = 'title';
-            title.textContent = this.utitle? this.utitle : 'No title provided';
+            title.textContent = this.utitle ? this.utitle : 'No title provided';
             textContainer.appendChild(title);
 
-            // Create text element
             const text = this.ownerDocument.createElement('p');
             text.className = 'text';
-            text.textContent = this.text? this.text : 'No text provided';
+            text.textContent = this.text ? this.text : 'No text provided';
             textContainer.appendChild(text);
 
-            // Create the container for banner image
             const imageContainer = this.ownerDocument.createElement('div');
             imageContainer.className = 'image-container';
             container.appendChild(imageContainer);
 
-            // Create image element
             const image = this.ownerDocument.createElement('img');
             image.className = 'image';
-            image.src = this.image? this.image : '';
+            image.src = this.image ? this.image : '';
             imageContainer.appendChild(image);
         }
     }
